8cf76ed60d0f0e849458b131d51c10e336546b86
Bryant Richards <dragonseyeimages@gmail.com>
2020-07-08 06:08:14 -0500

    set up initial scraper

---
+++ scraper.rb
@@ -0,0 +1,10 @@
+require 'nokogiri'
+require 'httparty'
+require 'byebug'
+
+def scraper
+  url = "https://www.indeed.com/"
+  unparsed_page = HTTParty.get(url)
+  parsed_page = Nokogiri::HTML(unparsed_page)
+  byebug
+end
