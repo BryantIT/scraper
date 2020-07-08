e4b0ce5b97a1c232f73584501ffb43beec637884
Bryant Richards <dragonseyeimages@gmail.com>
2020-07-08 06:45:25 -0500

    setup job variables

--- scraper.rb
+++ scraper.rb
@@ -3,8 +3,20 @@ require 'httparty'
 require 'byebug'
 
 def scraper
-  url = "https://www.indeed.com/"
+  url = "https://www.indeed.com/jobs?q=&l=Dickson%2C+TN"
   unparsed_page = HTTParty.get(url)
   parsed_page = Nokogiri::HTML(unparsed_page)
-  byebug
+  job_listings = parsed_page.css('div.jobsearch-SerpJobCard')
+  job_listings.each do |j|
+    job = {
+      title: job_listings.css('h2.title').text,
+      company: job_listings.css('a.turnstileLink').text,
+      location: job_listings.css('div.location').text,
+      description: job_listings.css('div.summary').text,
+
+    }
+    byebug
+  end
 end
+
+scraper
